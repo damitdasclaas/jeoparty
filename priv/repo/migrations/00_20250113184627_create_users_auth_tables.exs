d0484756fd2d65127b263f078fe97f97b7000e66
Claas Thore Klein <>
2025-01-13 19:48:14 +0100

    Add user and user token schemas with migrations
    - Updated `Jeoparty.Accounts.User` and `Jeoparty.Accounts.UserToken` modules to use binary IDs as primary keys.
    - Created migrations to establish `users` and `users_tokens` tables with appropriate fields and constraints.
    - Implemented unique indexes for email in the `users` table and context/token combinations in the `users_tokens` table.
    - Added new migration for `game_grids` and `question_cells` tables to support game functionality.

--- priv/repo/migrations/00_20250113184627_create_users_auth_tables.exs
+++ priv/repo/migrations/00_20250113184627_create_users_auth_tables.exs
@@ -3,10 +3,9 @@ defmodule Jeoparty.Repo.Migrations.CreateUsersAuthTables do
 
   def change do
     execute "CREATE EXTENSION IF NOT EXISTS citext", ""
-    execute "CREATE EXTENSION IF NOT EXISTS pgcrypto", ""
 
     create table(:users, primary_key: false) do
-      add :id, :uuid, primary_key: true, default: fragment("gen_random_uuid()")
+      add :id, :binary_id, primary_key: true
       add :email, :citext, null: false
       add :hashed_password, :string, null: false
       add :confirmed_at, :utc_datetime
@@ -17,8 +16,8 @@ defmodule Jeoparty.Repo.Migrations.CreateUsersAuthTables do
     create unique_index(:users, [:email])
 
     create table(:users_tokens, primary_key: false) do
-      add :id, :uuid, primary_key: true, default: fragment("gen_random_uuid()")
-      add :user_id, references(:users, type: :uuid, on_delete: :delete_all), null: false
+      add :id, :binary_id, primary_key: true
+      add :user_id, references(:users, type: :binary_id, on_delete: :delete_all), null: false
       add :token, :binary, null: false
       add :context, :string, null: false
       add :sent_to, :string
